7d9023285ec4134dc8dc8dd6749e221b24ec0e5e
Thanishkaa004 <82003727+Thanishkaa004@users.noreply.github.com>
2021-11-24 17:06:09 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canvas, backgroundImage;
+
+var questions;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  form = new Form()
+  form.start();
+}
+
+
+function draw(){
+  background("pink");
+
+}
